19f68b3b41f446b71e973746c854fe41c028cb92
Arash-11 <arashnawy@gmail.com>
2020-12-22 19:55:01 -0400

    updated heading files

--- src/heading/Heading.js
+++ src/heading/Heading.js
@@ -4,6 +4,7 @@ function Heading() {
   return (
     <div className="heading">
       <h1 className="heading__title">the shoppies</h1>
+      <button className="heading__button">My nominations</button>
     </div>
   );
 }
