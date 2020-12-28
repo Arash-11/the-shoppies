a4f1d9ca842ea92ef4b1a861e73a2d9bbf0386ef
Arash-11 <arashnawy@gmail.com>
2020-12-27 22:30:11 -0400

    updated styling for movie cards in nomination list

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,14 +3,18 @@ import React from 'react';
 function MovieCard(props) {
   return (
     <div className="movie-card">
-      <img src={props.imgSrc} className="movie-card__img" alt="Movie poster" />
+      <img
+        src={props.imgSrc}
+        className={`movie-card__img ${props.buttonLabel === "remove" ? "nominations--img" : ""}`}
+        alt="Movie poster"
+      />
       <div className="movie-card__text-content">
         <p className="movie-card__title">
           {props.title} <span>({props.year})</span>
         </p> 
         <button 
           onClick={() => props.selectMovie(props.id)}
-          className={`movie-card__button ${props.shouldFadeButton ? 'button--fade' : ''}`}
+          className={`movie-card__button ${props.shouldFadeButton ? "button--fade" : ""}`}
         >
           {props.buttonLabel}
         </button>
